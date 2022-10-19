4e960ea58501a014b54445bf584d1ed432f14567
moosimoosihan <segun408@gmail.com>
2022-10-19 17:43:06 +0900

    third commit

--- app/src/main/java/com/example/copyui/MainActivity.java
+++ app/src/main/java/com/example/copyui/MainActivity.java
@@ -7,10 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_search;
+    private ImageButton btn_search;
 
     @SuppressLint("WrongViewCast")
     @Override
