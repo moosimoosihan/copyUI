c8cfc198cdc15c43e7293b41638337dddf37dd7a
segun <segun408@gmail.com>
2022-10-27 17:15:58 +0900

    list view commit

--- app/src/main/java/com/example/copyui/HomeFragment.java
+++ app/src/main/java/com/example/copyui/HomeFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 
 public class HomeFragment extends Fragment {
